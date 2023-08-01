922ca647ddd3d29e0d05162ab7ca68604481f6c7
gabi-popoola <gabgab.popoola@gmail.com>
2023-08-01 14:47:30 +0100

    added initial pages and boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import PageWrapper from './components/PageWrapper/PageWrapper';
+
 const App = () => {
-	return <div></div>;
+	return (
+		<BrowserRouter>
+			<PageWrapper>
+				<Routes>
+					<Route exact path="/" Component={Home} />
+				</Routes>
+			</PageWrapper>
+		</BrowserRouter>
+	);
 };
 
 export default App;
