487bf59ef0569ec98deaee6e28b0456c176f57b4
gabi-popoola <gabgab.popoola@gmail.com>
2023-08-07 12:41:51 +0100

    installed prettier and eslint dependency

---
+++ .eslint.config.js
@@ -0,0 +1,9 @@
+export default {
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "plugins": ["react"],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  }
+}
