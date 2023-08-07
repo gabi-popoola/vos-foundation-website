2f7d2e5b0346ea34266b9940cb3204dd923eb050
Gabrielle Popoola <44483818+GabGab-rielle@users.noreply.github.com>
2023-08-07 21:12:04 +0100

    updated file structure

---
+++ src/app/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import PageWrapper from '../components/PageWrapper/PageWrapper';
+import Home from '../pages/Home/Home';
+import AboutUs from '../pages/AboutUs/AboutUs';
+import Donate from '../pages/Donate/Donate';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <PageWrapper>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="about-us" element={<AboutUs />} />
+          <Route exact path="donate" element={<Donate />} />
+        </Routes>
+      </PageWrapper>
+    </BrowserRouter>
+  );
+};
+
+export default App;
