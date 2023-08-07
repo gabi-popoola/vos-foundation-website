a78355c052f36467489d358392205b3495695f1a
Gabrielle Popoola <44483818+GabGab-rielle@users.noreply.github.com>
2023-08-07 22:30:07 +0100

    updated hero banner styling

--- src/pages/Home/HomeStyles.jsx
+++ src/pages/Home/HomeStyles.jsx
@@ -9,27 +9,30 @@ export const HeroBanner = styled.div`
   display: flex;
   justify-content: space-between;
   @media only screen and (min-width: 992px) {
-    padding-left: 10em;
   }
 `;
 export const HeroLeftSide = styled.div`
   margin: auto 1em;
+  padding: 1.5em 0;
   @media only screen and (min-width: 992px) {
-    width: 55%;
+    padding: 1.5em;
   }
 `;
 export const HeroRightSide = styled.div`
   display: none;
-  width: 45%;
+  width: 45vh;
   background-color: grey;
   @media only screen and (min-width: 992px) {
     display: block;
+    min-height: 50vh;
   }
 `;
-export const HeroTitle = styled.h1``;
+export const HeroTitle = styled.h1`
+  margin-top: 0;
+`;
 export const HeroText = styled.p`
   @media only screen and (min-width: 992px) {
-    max-width: 65%;
+    max-width: 65vh;
   }
 `;
 export const CoreWorkContainer = styled.div`
@@ -53,13 +56,15 @@ export const CoreWorkSubContainer = styled.div`
 export const CoreWorkTitle = styled.h1``;
 export const CoreWorkRightSide = styled.div`
   border: 1px solid red;
+  min-height: 70vh;
   @media only screen and (min-width: 992px) {
-    width: 50%;
+    width: 50vh;
   }
 `;
 export const CoreWorkLefttSide = styled.div`
   border: 1px solid red;
+  min-height: 55vh;
   @media only screen and (min-width: 992px) {
-    width: 50%;
+    width: 50vh;
   }
 `;
